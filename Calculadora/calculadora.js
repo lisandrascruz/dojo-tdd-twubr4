4c2864291acc0771d18239ef8b9a463aed50f676
Lisandra Cruz <lisansouza@gmail.com>
2022-03-02 19:36:18 -0300

    TDD Calculadora

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,5 +1,39 @@
-const Calculadora = () => {
+class Calculadora {
+  static soma(numero1, numero2) {
+    return numero1 + numero2;
+  }
 
+  static subtracao(numero1, numero2) {
+    return numero1 - numero2;
+  }
+
+  static mutiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+  }
+
+  static divisao(numero1, numero2) {
+    return numero1 / numero2;
+  }
+
+  static fatorial(numero) {
+    if (numero === 0 || numero === 1) {
+      return 0;
+    }
+
+    let resultado = numero;
+    for (let i = 1; i < numero; i++) {
+      resultado *= numero - i;
+    }
+
+    return resultado;
+  }
+
+  static fatorialDaSoma(numero1, numero2) {
+    const valorDaSoma = this.soma(numero1, numero2);
+    const resultado = this.fatorial(valorDaSoma);
+
+    return resultado;
+  }
 }
 
-module.exports = Calculadora
+module.exports = Calculadora;
--- Calculadora/calculadora.test.js
+++ Calculadora/calculadora.test.js
@@ -1,5 +1,36 @@
-const calculadora = require("./calculadora")
+const Calculadora = require("./calculadora");
 
 describe("Calculadora", () => {
+  const numero1 = 3;
+  const numero2 = 3;
 
-})
+  test("Deve realizar a soma dos dois valores recebidos", () => {
+    const resultado = Calculadora.soma(numero1, numero2);
+
+    expect(resultado).toBe(6);
+  });
+
+  test("Deve realizar a subtração dos dois valores recebidos", () => {
+    const resultado = Calculadora.subtracao(numero1, numero2);
+
+    expect(resultado).toBe(0);
+  });
+
+  test("Deve realizar a mutiplicação dos dois valores recebidos", () => {
+    const resultado = Calculadora.mutiplicacao(numero1, numero2);
+
+    expect(resultado).toBe(9);
+  });
+
+  test("Deve realizar a divisão dos dois valores recebidos", () => {
+    const resultado = Calculadora.divisao(numero1, numero2);
+
+    expect(resultado).toBe(1);
+  });
+
+  test("Deve realizar o fatorial da soma dos dois valores recebidos", () => {
+    const resultado = Calculadora.fatorialDaSoma(numero1, numero2);
+
+    expect(resultado).toBe(720);
+  });
+});
