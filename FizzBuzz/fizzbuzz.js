e5f657c6c4b8354380e565c9a05bed3b464bf796
Lisandra Cruz <lisansouza@gmail.com>
2022-03-02 19:34:27 -0300

    Refatoração do fizbuzz

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -1,21 +1,17 @@
 const multiploDe3 = (numero) => {
-    return numero%3===0
-}
+  return numero % 3 === 0;
+};
 
 const multiploDe5 = (numero) => {
-    return numero%5===0
-}
+  return numero % 5 === 0;
+};
 
 const fizzBuzz = (numero) => {
-    if(multiploDe3(numero) && multiploDe5(numero)) {
-        return "FizzBuzz"
-    } else if(multiploDe3(numero)) {
-        return "Fizz"
-    } else if(multiploDe5(numero)) {
-        return "Buzz"
-    } else {
-        return numero
-    }
-}
+  if (multiploDe3(numero) && multiploDe5(numero)) return "FizzBuzz";
+  else if (multiploDe3(numero)) return "Fizz";
+  else if (multiploDe5(numero)) return "Buzz";
 
-module.exports = fizzBuzz
+  return numero;
+};
+
+module.exports = fizzBuzz;
