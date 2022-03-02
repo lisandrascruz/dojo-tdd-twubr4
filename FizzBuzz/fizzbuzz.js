dabb5d691c3ecbc45c40c99c8fbac6a11e58a113
luzialeticia <leticia.luzia@thoughtworks.com>
2022-03-02 12:11:07 -0300

    Criação dos casos de teste e funcionalidades

---
+++ FizzBuzz/fizzbuzz.js
@@ -0,0 +1,21 @@
+const multiploDe3 = (numero) => {
+    return numero%3===0
+}
+
+const multiploDe5 = (numero) => {
+    return numero%5===0
+}
+
+const fizzBuzz = (numero) => {
+    if(multiploDe3(numero) && multiploDe5(numero)) {
+        return "FizzBuzz"
+    } else if(multiploDe3(numero)) {
+        return "Fizz"
+    } else if(multiploDe5(numero)) {
+        return "Buzz"
+    } else {
+        return numero
+    }
+}
+
+module.exports = fizzBuzz
---
+++ FizzBuzz/fizzbuzz.test.js
@@ -0,0 +1,38 @@
+const fizzBuzz = require("./fizzbuzz")
+
+describe("FizzBuzz", () => {
+
+    const multiplo3 = 9
+    const multiplo5 = 20
+    const multiplo3e5 = 30
+    const naoMultiplo = 31
+
+    test("Retornar 'Fizz' se o número recebido for múltiplo de 3", () => {
+        const resultadoEsperado = "Fizz"
+        const resultado = fizzBuzz(multiplo3)
+    
+        expect(resultado).toBe(resultadoEsperado)
+    })
+    
+    test("Retornar 'Buzz' se o número recebido for múltiplo de 5", () => {
+        const resultadoEsperado = "Buzz"
+        const resultado = fizzBuzz(multiplo5)
+    
+        expect(resultado).toBe(resultadoEsperado)
+    })
+    
+    test("Retornar 'FizzBuzz' se o número recebido for múltiplo de 3 e de 5", () => {
+        const resultadoEsperado = "FizzBuzz"
+        const resultado = fizzBuzz(multiplo3e5)
+    
+        expect(resultado).toBe(resultadoEsperado)
+    })
+    
+    test("Retornar o próprio número recebido se não for múltiplo de ambos", () => {
+        const resultadoEsperado = naoMultiplo
+        const resultado = fizzBuzz(naoMultiplo)
+    
+        expect(resultado).toBe(resultadoEsperado)
+    })
+
+})
